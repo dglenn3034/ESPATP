63c0f8b9c59174bf2188284e2c84e3d39298f0d6
David Glenn <dglenn@geocue.com>
2018-05-18 14:31:23 -0500

    Force basemap upload

--- Scripts/BasemapSanity/Script1526585577426.groovy
+++ Scripts/BasemapSanity/Script1526585577426.groovy
@@ -20,6 +20,8 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+/* begin here */
+
 WebUI.openBrowser('')
 
 WebUI.maximizeWindow()
